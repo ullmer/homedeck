3e1da6df085f861fc14e5e566d16428ebf9c518d
redphx <96280+redphx@users.noreply.github.com>
2025-04-20 10:04:26 +0700

    Create install.sh

---
+++ guides/linux/install.sh
@@ -0,0 +1,20 @@
+# USB HID
+apt install -y udev usbutils libusb-1.0-0-dev libudev-dev
+# Python3 with venv support
+apt install -y python3-venv
+# Tools for HomeDeck
+apt install -y libcairo2 optipng
+
+mkdir -p /app
+# Download HomeDeck
+git clone https://github.com/redphx/homedeck.git
+
+# Create custom venv
+python3 -m venv /app/homedeck-venv
+# Install HomeDeck
+source /app/homedeck-venv/bin/activate && cd /app/homedeck && pip install -e .
+
+# Run HomeDeck at startup
+( crontab -l 2>/dev/null; echo "@reboot /bin/bash -c 'source /app/homedeck-venv/bin/activate && python /app/homedeck/server.py'" ) | crontab -
+
+print('Installed HomeDeck successfully!')
